22e10762d410e84c40e193afa0c6eb041b19c5d4
Mahfuz Ahmed <128321890+mahfuzmahi@users.noreply.github.com>
2025-06-02 16:51:33 -0700

    Created elements folder and started working on CameraFeed (wip)

---
+++ eye-guide/src/elements/CameraFeed.js
@@ -0,0 +1,40 @@
+import React, {useRef, useEffect} from 'react';
+
+function CameraFeed() {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const startCamera = async() => {
+            try {
+                const streamCamera = await navigator.mediaDevices.getUserMedia({
+                    video: {
+                        facingMode: {ideal: 'environment'}
+                    },
+                    audio: false
+                })
+
+                if(videoRef.temp) {
+                    videoRef.temp.srcObject = streamCamera;
+                }
+            } catch(error) {
+                console.log("Unable to access camera", error);
+                alert("Camera access required in order for EyeGuide to work!");
+            }
+        };
+        startCamera(); 
+    });
+
+    return (
+        <video
+            ref={videoRef}
+            autoPlay
+            playsInline
+
+            style = {{
+                width: '100vw', height: '100vh', objectFit: 'cover', display: 'block'
+            }}
+        />
+    );
+}
+
+export default CameraFeed;
