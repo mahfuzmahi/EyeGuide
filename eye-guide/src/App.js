59f846d0fc61c9a8329b9dd2ace62c261c6991bf
Mahfuz Ahmed <128321890+mahfuzmahi@users.noreply.github.com>
2025-06-04 17:52:01 -0700

    Moved CameraFeed to App.js

--- eye-guide/src/App.js
+++ eye-guide/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const videoCam = useRef(null);
+
+    useEffect(() => {
+        const startCam = async () => {
+            try {
+                const camStream = await navigator.mediaDevices.getUserMedia({
+                    video: {
+                        facingMode: {ideal: 'environment'} // access to the back camera
+                    },
+                    audio: false
+                });
+
+                if(videoCam.current) {
+                    videoCam.current.srcObject = camStream;
+                }
+            } catch (error) {
+                console.log("Unable to access camera", error);
+                alert("Camera access required in order for EyeGuide to work!");
+            }
+        };
+        startCam();
+    }, []);
+
+    return (
+        <video>
+            ref = {videoCam}
+            autoPlay
+            playsInLine
+
+            style = {{
+                width: '100vw',
+                height: '100vh',
+                objectFit: 'cover',
+                display: 'block'
+            }}
+        </video>
+    );
 }
 
 export default App;
