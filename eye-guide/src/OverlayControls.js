f0c8ad3aab54e79f8052a1e6cb68141c828b8965
Mahfuz Ahmed <128321890+mahfuzmahi@users.noreply.github.com>
2025-06-17 20:27:56 -0700

    Added bottom layer buttons for Summarize, Mute and Pause

--- eye-guide/src/OverlayControls.js
+++ eye-guide/src/OverlayControls.js
@@ -1,29 +1,66 @@
 import React from "react";
 
 function OverlayControls({
-
+    onPauseClick,
+    onVoiceClick,
+    onSummaryClick,
+    isMuted,
+    isPaused
 }) {
+    const PauseButton = () => {
+        if (isPaused) {
+            return "Resume";
+        } else {
+            return "Pause";
+        }
+    }
+
+    const MuteButton = () => {
+        if (isMuted) {
+            return "Unmute";
+        } else {
+            return "Mute";
+        }
+    }
+
     return (
         <>
-        <div>
+        <div
             style = {{
                 width: '100%',
                 height: "50px",
                 backgroundColor: '#28282B',
-                top: 0,
+                bottom: 0,
                 left: 0,
                 position: 'fixed',
                 display: 'flex',
                 alignItems: 'center',
-                justifyContent: 'space-around',
-                
+                justifyContent: 'space-evenly',
+                color: 'white',
+                fontsize: '16px',
+                zIndex: 10,
             }}
-        </div>
-        <div>
-
+        >
+            <button onClick = {onPauseClick} style = {button}>
+            {PauseButton()}
+            </button>
+            <button onClick = {onSummaryClick} style = {button}>
+            Summarize
+            </button>
+            <button onClick = {onVoiceClick} style = {button}>
+            {MuteButton()}
+            </button>
         </div>
         </>
     );
 }
+
+const button =  {
+    color: 'white',
+    fontsize: '16px',
+    cursor: 'pointer',
+    padding: '8px 12px',
+    borderRadius: '6px'
+};
 
 export default OverlayControls;
