05a03cef12a408556691a6c466a0c7b6afd738a4
Mahfuz Ahmed <128321890+mahfuzmahi@users.noreply.github.com>
2025-06-24 18:25:16 -0700

    Added guide based on logged objects

---
+++ eye-guide/src/MemoryGuide.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useRef} from "react";
+
+function MemoryGuide({detectedObjects, speak}) {
+    const log = useRef([]);
+    const time = useRef(Date.now());
+    const stationary = 15000;
+    const seenAgain = 30000;
+
+    useEffect(() => {
+        if(detectedObjects || detectedObjects.length === 0) {
+            return;
+        }
+
+        const now = Date.now();
+
+        log.current = log.current.filter(
+            (entry) => now - entry.timestamp < 120000
+        );
+    })
+}
