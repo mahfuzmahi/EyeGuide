59d3d11501eb00343ac7c7ff25929361ae67e930
Mahfuz Ahmed <128321890+mahfuzmahi@users.noreply.github.com>
2025-06-26 17:23:51 -0700

    Takes a score based off object and makes a guess with the best score

--- eye-guide/src/SceneryGuesser.js
+++ eye-guide/src/SceneryGuesser.js
@@ -16,5 +16,28 @@ function SceneryGuesser({detectedObjects, speak}) {
         if(detectedObjects || detectedObjects.length === 0) {
             return;
         }
-    })
+
+        const now = Date.now();
+        if(now - time.current < 30000) {
+            return;
+        }
+
+        const oClass = detectedObjects.map(o => o.class);
+        const score = {};
+
+        for(const [s, i] of Object.entries(map)) {
+            score[s] = i.filter(item => oClass.includes(item).length);
+        }
+
+        const best = Object.entries(score).sort((a,b) => b[1] - a[1][0]);
+
+        if(best && best[1] >= 2 && best[0] !== lastScene.current) {
+            speak(`You seem to be in a ${best[0]}`);
+            lastScene.current = best[0];
+            time.current = now;
+        }
+    }, [detectedObjects, speak])
+    return null;
 }
+
+export default SceneryGuesser;
