55dcb3a5e743335bddacbbcdfa5d6ed6548e3501
Mahfuz Ahmed <128321890+mahfuzmahi@users.noreply.github.com>
2025-06-29 16:16:59 -0700

    Speech queue replaced for smoother narration

--- eye-guide/src/components/SpeechQueue.js
+++ eye-guide/src/components/SpeechQueue.js
@@ -1,45 +1,13 @@
-const q = [];
-let time = null;
-
 export const speakObject = (text) => {
-    if(!text) {
-        return ;
+    if (!text) {
+        return;
     }
-    q.push(text);
 
-    if(time) {
-        clearTimeout(time);
+    if (window.speechSynthesis.speaking) {
+        window.speechSynthesis.cancel();
     }
-    time = setTimeout(() => {
-        if(q.length === 0) {
-            return;
-        }
 
-        const count = {};
-        q.forEach(i => {
-            if(count[i]) {
-                count[i] += 1;
-            } else {
-                count[i] = 1;
-            }
-        });
-
-        const p = [];
-        for (const key in count) {
-            const c = count[key];
-            let phrase = c + " " + key;
-
-            if(c > 1) {
-                phrase += "s";
-            }
-            p.push(phrase);
-        }
-
-        const utter = new SpeechSynthesisUtterance("You are in front of " + p.join(", "));
-        utter.rate = 1;
-        window.speechSynthesis.speak(utter);
-
-        q.length = 0;
-        time = null;
-    }, 5000);
-}
+    const utter = new SpeechSynthesisUtterance(text);
+    utter.rate = 1;
+    window.speechSynthesis.speak(utter);
+};
